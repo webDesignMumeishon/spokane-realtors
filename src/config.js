75bce1e742a537ad276fee82ddc71920a3c12c2d
tomasMutual <tomas@mutual.app>
2024-06-02 22:29:37 -0300

    fix metadata

--- src/config.js
+++ src/config.js
@@ -1,10 +1,8 @@
 module.exports.SITE = {
   name: 'Spokane Realtors',
-
   origin: 'https://spokane-realtors.com/',
   basePathname: '/',
   trailingSlash: false,
-
-  title: 'Spokane Realtors',
-  description: 'Your Trusted Real Estate Experts',
+  title: 'Spokane Realtors - Your Trusted Real Estate Experts',
+  description: 'At Spokane Realtors, we offer a range of services including home buying, home selling, real estate investment, relocation assistance, and property management.',
 };
